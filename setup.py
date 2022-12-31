755ac0c6fb988e9bcfd8ad88f991c624c3b6aaca
Burton-David <dgburton@pm.me>
2022-12-31 13:43:42 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name="completeNLP",
+    version="0.1.0",
+    author="David Burton",
+    author_email="dgburton@pm.me",
+    description="A library for natural language processing tasks",
+    long_description="A library for natural language processing tasks, including preprocessing, feature extraction, and machine learning",
+    url="https://github.com/Burton-David/NLP-Natural-Language-Processing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
